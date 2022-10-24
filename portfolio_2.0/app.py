6d6c3471b203ffe34e18749559e73c65c8d7c419
Gabriela Barbosa <gabrielasbarbosa4@gmail.com>
2022-10-24 08:19:28 -0300

    criação de novas rotas flask e novas páginas dos projetos

--- portfolio_2.0/app.py
+++ portfolio_2.0/app.py
@@ -21,6 +21,21 @@ def projects():
 def contact():
     return render_template('contact.html')
 
+@app.route('/project_1')
+@app.route('/projeto_1')
+def projeto_1():
+    return render_template('project_1.html')
+
+@app.route('/project_2')
+@app.route('/projeto_2')
+def projeto_2():
+    return render_template('project_2.html')
+
+@app.route('/project_3')
+@app.route('/projeto_3')
+def projeto_3():
+    return render_template('project_3.html')
+
 
 
 
