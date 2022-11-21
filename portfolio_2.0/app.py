daaa0ba14b041a32fac678e2d8fe6d2e23e3cf34
Gabriela Barbosa <gabrielasbarbosa4@gmail.com>
2022-11-20 23:30:39 -0300

    projeto para entrega dia 21/11, mas ainda necessitando de pequenas melhorias

--- portfolio_2.0/app.py
+++ portfolio_2.0/app.py
@@ -4,37 +4,46 @@ app = Flask('__name__')
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('public/index.html')
 
 @app.route('/about')
 @app.route('/sobre')
 def about():
-    return render_template('about.html')
+    return render_template('public/about.html')
 
 @app.route('/projects')
 @app.route('/projetos')
 def projects():
-    return render_template('projects.html')
+    return render_template('public/projects.html')
 
 @app.route('/contact')
 @app.route('/contato')
 def contact():
-    return render_template('contact.html')
+    return render_template('public/contact.html')
 
 @app.route('/project_1')
 @app.route('/projeto_1')
 def projeto_1():
-    return render_template('project_1.html')
+    return render_template('public/project_1.html')
 
 @app.route('/project_2')
 @app.route('/projeto_2')
 def projeto_2():
-    return render_template('project_2.html')
+    return render_template('public/project_2.html')
 
 @app.route('/project_3')
 @app.route('/projeto_3')
 def projeto_3():
-    return render_template('project_3.html')
+    return render_template('public/project_3.html')
+
+@app.route('/project_4')
+@app.route('/projeto_4')
+def projeto_4():
+    return render_template('public/project_4.html')
+
+@app.route('/erro404')
+def erro404():
+    return render_template('public/erro_404.html')
 
 
 if __name__ == 'main':
